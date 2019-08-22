917e7f6cc3336e34b0ab48ff3913327b65cc4cdf
Negin Basiri <negin.basiri@empired.com>
2019-08-22 13:59:58 +1000

    started modal

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import '../styles/_modal.scss';
 
-const Modal = () => {
-
-}
+const Modal = props => (
+	<div className="modal-wrapper">
+		<div className="modal-header">
+			{' '}
+			<span className="modal-header-close" onClick={props.close}>
+				×
+			</span>
+		</div>
+		<div className="modal-body">
+			<p>{props.children}</p>
+		</div>
+	</div>
+);
 
 export default Modal;
